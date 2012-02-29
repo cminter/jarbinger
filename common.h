4983401e40e5162700095432e906abeea873d437
cminter <corey.minter@gmail.com>
2012-02-29 15:24:27 -0800

    added comments/documentation (more to do)

--- common.h
+++ common.h
@@ -14,11 +14,20 @@ namespace Jarbinger {
 
 typedef u_int64_t U64;
 
+/**
+ * contains info to output usage message
+ */
 class Usage {
 public:
+    /**
+     * output usage message
+     */
     virtual void output()= 0;
 };
 
+/**
+ * exception with message, usage object, and exit code
+ */
 class Exception {
 public:
     Exception(std::string message, Usage* usage= NULL, int exitCode= 1)
@@ -44,10 +53,44 @@ private:
     int _exitCode;
 };
 
+/**
+ * copy internal data (expected to be an archive) to a file and un-archive it
+ * @param toolName tool name used in file path
+ * @param dataAddr data address
+ * @param dataSize data size
+ * @return path to data directory
+ */
 std::string unArchive(const std::string& toolName, char* dataAddr, const U64 dataSize);
+
+/**
+ * copy internal data to file
+ * @param filename file name
+ * @param dataAddr data address
+ * @param dataSize data size
+ */
 void writeToFile(const std::string& filename, char* dataAddr, const U64 dataSize);
+
+/**
+ * get current working directory.
+ * uses cwd() 
+ * @return directory
+ */
 std::string getCwd();
+
+/**
+ * set current working directory.
+ * uses chdir()
+ * @param dirName directory
+ * @return result of chdir()
+ */
 int setCwd(const std::string& dirName);
+
+/**
+ * replace all occurences of string with another
+ * @param str string to be modified
+ * @param oldText search text
+ * @param newText replacement text
+ */
 void replace(std::string& str, const std::string& oldText,
         const std::string& newText);
 
--- jvm.cpp
+++ jvm.cpp
@@ -21,18 +21,25 @@ using std::vector;
 
 //////////////////////////////////////////////////////////////////////
 
-static JNIEnv* createJVM(const string& classpath);
+/**
+ * create Java Virtual Machine (JVM) with given classpath and JVM opts
+ */
+static JNIEnv* createJVM(const string& classpath, const string& jvmOpts);
 
 void startJVM(const string& mainJarFilename,
         const string& mainClassName, vector<string>& vArgs) {
-    //TODO: use JVM_OPTS
+    // construct classpath and create JVM
     string pkgSep(".");
     string pathSep("/");
     string pkgMainClassName(mainClassName);
     replace(pkgMainClassName, pathSep, pkgSep);
     string pathMainClassName(mainClassName);
     replace(pathMainClassName, pkgSep, pathSep);
-    JNIEnv* env= createJVM(mainJarFilename);
+    string jvmOpts("");
+    //TODO: prepare JVM_OPTS
+    JNIEnv* env= createJVM(mainJarFilename, jvmOpts);
+
+    // find main class and main() method
     jclass mainClass= env->FindClass(pathMainClassName.c_str());
     if (0 == mainClass) {
         if (env->ExceptionOccurred()) env->ExceptionDescribe();
@@ -44,6 +51,8 @@ void startJVM(const string& mainJarFilename,
         if (env->ExceptionOccurred()) env->ExceptionDescribe();
         throw Exception("unable to find main method in '"+ pkgMainClassName +"'");
     }
+
+    // prepare args and invoke main()
     jobjectArray args= env->NewObjectArray(vArgs.size(),
             env->FindClass("java/lang/String"), 0);  
     int argNo= 0;
@@ -58,22 +67,26 @@ void startJVM(const string& mainJarFilename,
 
 //////////////////////////////////////////////////////////////////////
 
-static JNIEnv* createJVM(const string& classpath) {
+static JNIEnv* createJVM(const string& classpath, const string& jvmOpts) {
     JavaVM* jvm;
     JNIEnv* env;
     JavaVMInitArgs args;
     JavaVMOption options[1];
 
+    // prepare JVM options
     args.version= JNI_VERSION_1_6;
     args.nOptions= 1;
     string classpathOpt("-Djava.class.path="+ classpath);
     options[0].optionString= strdup(classpathOpt.c_str());
+    //TODO: fill jvmOpts into options
     args.options= options;
     args.ignoreUnrecognized= JNI_FALSE;
 
+    // create JVM
     int createStatus= JNI_CreateJavaVM(&jvm, (void**)&env, &args);
     if (createStatus < 0) {
-        //TODO: error handling
+        //TODO: show options in message
+        throw Exception("unable to create JVM");
     }
     return env;
 }
